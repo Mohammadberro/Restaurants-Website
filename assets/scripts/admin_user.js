5349177d97f4d3bb0f493f93f2abf31733be84a0
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-06 15:59:27 +0200

    first commit

---
+++ assets/scripts/admin_user.js
@@ -0,0 +1,16 @@
+const AdminName=document.getElementById("admin-name");
+const UserName=document.getElementById("user-name");
+
+// buttons for admin
+const AddUser=document.getElementById("add-user");
+const RemoveUser=document.getElementById("Remove-user");
+const AddRest=document.getElementById("add-rest");
+const RemoveRest=document.getElementById("remove-rest");
+
+// buttons for user
+const OpenFav=document.getElementById("open-fav");
+const EditUserName=document.getElementById("edit-name");
+const EditPass=document.getElementById("edit-pass");
+
+
+
