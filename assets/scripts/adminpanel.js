16231b08910890a359f272ec58028bc7607c644e
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-06 22:29:07 +0200

    fixing some bugs

---
+++ assets/scripts/adminpanel.js
@@ -0,0 +1,8 @@
+const AddUser=document.getElementById("add-user");
+const RemoveUser=document.getElementById("Remove-user");
+const AddRest=document.getElementById("add-rest");
+const RemoveRest=document.getElementById("remove-rest");
+
+
+
+
