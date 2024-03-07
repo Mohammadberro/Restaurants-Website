6f0c39a748f31e8adec779db627300d696ea6e40
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-03-07 09:59:19 +0200

    finalized

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -312,7 +312,6 @@ let res_menus = [
 localStorage.setItem("img_src", JSON.stringify(img_src));
 localStorage.setItem("res_name", JSON.stringify(res_name));
 localStorage.setItem("res_menus", JSON.stringify(res_menus));
-localStorage.setItem("login", "true");
 
 window.onload = () => {
   let url = window.location.href;
@@ -518,3 +517,191 @@ function DisplayRest(url) {
   menu.appendChild(list);
 }
 
+//Ashraf
+// buttons to open the hidden panels
+const AddUser = document.getElementById("popup-adduser");
+const ViewUser = document.getElementById("popup-viewuser");
+const AddRest = document.getElementById("popup-addrest");
+const ViewRest = document.getElementById("popup-viewrest");
+
+const adminPanel = document.getElementById("admin-panel");
+const userPanel = document.getElementById("user-panel");
+
+const addUserpanel = document.getElementById("adduser-panel");
+const addRestpanel = document.getElementById("addrest-panel");
+const viewUserpanel = document.getElementById("viewuser-panel");
+const viewRestpanel = document.getElementById("viewrest-panel");
+
+const editusernamePanel=document.getElementById("edit-username-panel");
+
+
+const Editusername=document.getElementById("popup-editname");
+const Edituserpass=document.getElementById("popup-editpass");
+
+// events to open the hidden panels
+AddUser.addEventListener("click", function () {
+  Showhidden(addUserpanel);
+});
+AddRest.addEventListener("click", function () {
+  Showhidden(addRestpanel);
+});
+ViewRest.addEventListener("click", function () {
+  Showhidden(viewRestpanel);
+});
+ViewUser.addEventListener("click", function () {
+  Showhidden(viewUserpanel);
+});
+
+let user = { name: "ashraf", password: "" };
+let users = [user,user];
+// let users = [];
+localStorage.setItem("users", JSON.stringify(users));
+
+function Showhidden(hidden) {
+  hidden.classList.remove("hidden");
+  adminPanel.classList.remove("hidden");
+  userPanel.classList.remove("hidden");
+}
+
+function Hidepanels(){
+addUseropanel.classList.add("hidden");
+addRestpanel.classList.add("hidden");
+viewRestpanel.classList.add("hidden");
+viewUserpanel.classList.add("hidden");
+adminPanel.classList.add("hidden");
+}
+function Adduser() {
+  let username = document.getElementById("add-username").value;
+  let password = document.getElementById("add-userpass").value;
+  let name = JSON.parse(localStorage.getItem("users") || "[]");
+
+  let user = { name: username, password: password };
+  name.push(user);
+
+  localStorage.setItem("users", JSON.stringify(name));
+  Hidepanels();
+}
+
+function Viewuser() {
+  let Viewcontainer = document.getElementById("viewUser-container");
+  Viewcontainer.innerHTML="";
+  let namesArray = JSON.parse(localStorage.getItem("users") || "[]");
+  for (let i = 0; i <= namesArray.length; i++) {
+    names = namesArray[i];
+    Viewcontainer.innerHTML += `<div><h2>${names.name}</h2><button id="${names.name}"onclick="Removeuser(this)">remove</button></div>`
+  }
+}
+
+function Removeuser(event){
+  let name = JSON.parse(localStorage.getItem("users") || "[]");
+  let index = 0;
+  for(let i =0; i<name.length;i++){
+    if(event.id == name[i].name){
+      index = i;
+    }
+    name[index].name = "";
+    name[index].password = ""
+    localStorage.setItem("users", JSON.stringify(name));
+  }
+}
+
+function Adduser() {
+  let username = document.getElementById("add-username").value;
+  let password = document.getElementById("add-userpass").value;
+  let name = JSON.parse(localStorage.getItem("users") || "[]");
+
+  let user = { name: username, password: password };
+  name.push(user);
+
+  localStorage.setItem("users", JSON.stringify(name));
+  Hidepanels();
+}
+
+function Viewrest() {
+  let Viewcontainer = document.getElementById("viewrest-container");
+  Viewcontainer.innerHTML="";
+  let namesArray = JSON.parse(localStorage.getItem("res_name") || "[]");
+  for (let i = 0; i <= namesArray.length; i++) {
+    names = namesArray[i];
+    Viewcontainer.innerHTML += `<div><h2>${names}</h2><button id="${names}"onclick="RemoveRest(this)">remove</button></div>`
+  }
+}
+function RemoveRest(event){
+  let name = JSON.parse(localStorage.getItem("res_name") || "[]");
+  let index = 0;
+  for(let i =0; i<name.length;i++){
+    if(event.id == name[i].name){
+      index = i;
+    }
+    name[index].name = "";
+    name[index].password = ""
+    localStorage.setItem("users", JSON.stringify(name));
+  }
+}
+function Removeuser(event){
+  let name = JSON.parse(localStorage.getItem("users") || "[]");
+  let index = 0;
+  for(let i =0; i<name.length;i++){
+    if(event.id == name[i].name){
+      index = i;
+    }
+    name[index].name = "";
+    name[index].password = ""
+    localStorage.setItem("users", JSON.stringify(name));
+  }
+}
+
+
+//mohammad
+function signUp(){
+  let name = document.getElementById("username");
+  let password = document.getElementById("password");
+  let confirmPassword = document.getElementById("confirm-password");
+  // let userList = JSON.parse(localStorage.getItem("users") || "[]");
+  let userList = [];
+  if(password.value == confirmPassword.value){
+    let user={
+      name: name.value,
+      password: password.value,
+    }
+    userList.push(user);
+    localStorage.setItem("users", JSON.stringify(userList));
+    let url = "../pages/userpage.html?u=" + name.value;
+    window.location.href = url;
+    }
+  }
+  
+  
+  function login(){
+    let name = document.getElementById("loginusername");
+    let password = document.getElementById("loginpassword");
+    let userList = JSON.parse(localStorage.getItem("users") || "[]");
+    let checkUser = false; 
+    let adminName = "admin";
+    let adminPass = "admin";
+  
+    if (name.value == adminName && password.value == adminPass){
+      localStorage.setItem("login","true")
+      let url = "../pages/adminpage.html?a=" + name.value;
+      window.location.href = url;
+    }
+  
+    else{
+      for(let i=0; i < userList.length; i++){
+        if(name.value == userList[i].name 
+          && password.value == userList[i].password){
+            checkUser = true;
+            break
+        }
+      }
+      if (checkUser){
+        let url = "../pages/userpage.html?u=" + name.value;
+        window.location.href = url;
+      }
+      else{
+        // console.log(alert);
+        window.alert("wrong username or password")
+      }
+    }
+  }
+  
--- assets/scripts/userpanel.js
+++ assets/scripts/userpanel.js
@@ -1,3 +1,4 @@
 const OpenFav=document.getElementById("open-fav");
 const EditUserName=document.getElementById("edit-name");
 const EditPass=document.getElementById("edit-pass");
+
